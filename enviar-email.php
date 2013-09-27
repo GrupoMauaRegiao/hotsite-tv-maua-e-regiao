927ba30dc793892e036a7532c5ed4c5cba9f971c
Marcos Garcia de Lima <marckfree@gmail.com>
2013-09-27 11:52:42 -0300

    Implementa criação de listas ao efetuar cadastro no hotsite

--- enviar-email.php
+++ enviar-email.php
@@ -1,43 +1,63 @@
 <?php
-  function criarListaEmails($enderecoEmail, $arquivo) {
-    if ($enderecoEmail) {
-      if (file_exists($arquivo)) {
-        $hdlr = fopen($arquivo, "a+r") or print("O arquivo não pode ser aberto.");
-        $conteudo = fread($hdlr, filesize($arquivo));
+// Converte encoding "X" para UTF-8
+function mudarEncodingParaUtf8($texto) {
+  $encodingAtual = mb_detect_encoding($texto, "auto");
+  $texto = iconv($encodingAtual, 'UTF-8', $texto);
+  return $texto;
+}
 
-        if (!preg_match("/$enderecoEmail/i", $conteudo)) {
-          if (filter_var($enderecoEmail, FILTER_VALIDATE_EMAIL) !== FALSE) {
-            if (PATH_SEPARATOR == ";") {
-              $quebraLinha = "\r\n";
-            } else {
-              $quebraLinha = "\n";
-            }
+function enviarEmailsCriarListas($email, $nome, $arqEmails, $arqNomes) {
+  if ($email) {
+    if (file_exists($arqEmails)) {
+      $hdlrEmail = fopen($arqEmails, "a+r") or print("O arquivo não pode ser aberto.");
+      $conteudoEmail = fread($hdlrEmail, filesize($arqEmails));
 
-            $destinatario = "marckfree@gmail.com";
-            // $destinatario = "web@revistamaua.com.br";
-            $assunto = "TV MAUÁ E REGIÃO - QUERO SER O PRIMEIRO";
-            $nome = ucwords($_GET["nome"]);
-            $conteudo = 
-              '<p><b>Nome:</b> ' . $nome . '</p>
-               <p><b>E-mail:</b> ' . $enderecoEmail . '</p>
-               <p><b>Assunto:</b> ' . $assunto . '</p>';
-               
-            $headers .= "MIME-Version: 1.1" . $quebraLinha;
-            $headers .= "Content-type: text/html; charset=utf-8" . $quebraLinha;
-            $headers .= "From: " . $enderecoEmail . $quebraLinha;
-            
-            if (!mail($destinatario, $assunto, $conteudo, $headers , "-r" . $destinatario)) {
-              mail($destinatario, $assunto, $conteudo, $headers);
-            }
+      if (!preg_match("/$email/i", $conteudoEmail)) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) !== FALSE) {
+          if (PATH_SEPARATOR == ";") {
+            $quebraLinha = "\r\n";
+          } else {
+            $quebraLinha = "\n";
+          }
 
-            fwrite($hdlr, $enderecoEmail . ", ");
-            fclose($hdlr);
+          $destinatario = "dev@marcker.net";
+          // $destinatario = "web@revistamaua.com.br";
+          $assunto = "TV MAUÁ E REGIÃO - QUERO SER O PRIMEIRO";
+          $nome = ucwords($nome);
+          $conteudo = 
+            '<p><b>Nome:</b> ' . $nome . '</p>
+             <p><b>E-mail:</b> ' . $email . '</p>
+             <p><b>Assunto:</b> ' . $assunto . '</p>';
+             
+          $headers .= "MIME-Version: 1.1" . $quebraLinha;
+          $headers .= "Content-type: text/html; charset=utf-8" . $quebraLinha;
+          $headers .= "From: " . $email . $quebraLinha;
+
+          if (!mail($destinatario, $assunto, $conteudo, $headers , "-r " . $destinatario)) {
+            mail($destinatario, $assunto, $conteudo, $headers);
+          }
+
+          fwrite($hdlrEmail, $email . ", ");
+          fclose($hdlrEmail);
+
+          // Adiciona nomes a lista
+          if (file_exists($arqNomes)) {
+            $hdlrNome = fopen($arqNomes, "a+r") or print("O arquivo não pode ser aberto.");
+            fwrite($hdlrNome, mudarEncodingParaUtf8($nome) . "\n");
+            fclose($hdlrNome);
           }
         }
       }
     }
   }
+}
 
-  criarListaEmails(strtolower($_GET["email"]), '/home/marc11/public_html/hot-tv/lista-de-inscricoes.txt');
+$path = '/home/marc11/public_html/hot-tv/';
+criarListaEmails(
+  strtolower($_GET["email"]),
+  $_GET["nome"],
+  $path . "lista-de-emails.txt",
+  $path . "lista-de-nomes.txt"
+);
 
 ?>
