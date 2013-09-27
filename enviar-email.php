ca94c82bb477c0ee197e3090003658f964300c2a
Marcos Garcia de Lima <marckfree@gmail.com>
2013-09-27 11:59:32 -0300

    Cria arquivo utils.php

--- enviar-email.php
+++ enviar-email.php
@@ -1,10 +1,5 @@
 <?php
-// Converte encoding "X" para UTF-8
-function mudarEncodingParaUtf8($texto) {
-  $encodingAtual = mb_detect_encoding($texto, "auto");
-  $texto = iconv($encodingAtual, 'UTF-8', $texto);
-  return $texto;
-}
+include "utils.php";
 
 function enviarEmailsCriarListas($email, $nome, $arqEmails, $arqNomes) {
   if ($email) {
---
+++ utils.php
@@ -0,0 +1,8 @@
+<?php
+// Converte encoding "X" para UTF-8
+function mudarEncodingParaUtf8($texto) {
+  $encodingAtual = mb_detect_encoding($texto, "auto");
+  $texto = iconv($encodingAtual, 'UTF-8', $texto);
+  return $texto;
+}
+?>
